2c27e817b58c20ab1b7d78fd977fe8102201f2ba
Ashwin Vaithianathan <ashwinmelo12@gmail.com>
2013-01-31 08:51:35 -0700

    Created palindrome class and started iterative method

---
+++ palindrome.java
@@ -0,0 +1,57 @@
+/**
+ * palindrome
+ *
+ * 		determines if a word is a palindrome or not
+ * 		
+ */
+public class palindrome {
+	/**
+	 * main
+	 * 
+	 * 
+	 * @param args - args[0] is the string entered in to check if it is a palindrome or not.
+	 */
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.out.println("Please type a word the next time you run this program.");
+			System.exit(0);
+		}
+
+		String s = (args[0]);
+		
+
+		System.out.println(s);
+		
+		// iterativePalindrome((args[0]));
+	}
+
+
+	/**
+	 * iterativePalindrome
+	 * 		iteratively determines if s is a palindrome or not
+	 * 	
+	 * 	@param String s
+	 *
+	 * @return true or false
+	 */
+	public static boolean iterativePalindrome(String s) {
+		return true;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
